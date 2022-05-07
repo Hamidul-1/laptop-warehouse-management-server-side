f51f8275efb5a708e845ff0b1fed38d9e0e75bc9
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-07 19:05:52 +0600

    first commit

--- index.js
+++ index.js
@@ -7,22 +7,23 @@ const port = process.env.PORT || 5000;
 //dbuser1
 //GFLNZf96gbWyY1bL
 
-
-const uri = "mongodb+srv://dbuser1:GFLNZf96gbWyY1bL@cluster0.kheel.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db connected');
-    // perform actions on the collection object
-    client.close();
-});
-
-
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwx9p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('Laptop warehouse db connected');
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Running Genius Server');
