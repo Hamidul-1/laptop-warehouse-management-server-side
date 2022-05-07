b264dd4499f0367b87b3b319418eac06f85252e1
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-08 01:07:54 +0600

    post added

--- index.js
+++ index.js
@@ -13,16 +13,53 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwx9p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kheel.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
+/* client.connect(err => {
+    console.log('Computer');
     const collection = client.db("test").collection("devices");
-    console.log('Laptop warehouse db connected');
     // perform actions on the collection object
     client.close();
-});
+}); */
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('laptopWarehouse').collection('laptopItem');
+
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await serviceCollection.findOne(query);
+            res.send(item);
+        });
+
+        // POST
+        app.post('/item', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        
+
+    }
+    finally {
+
+    }
+}
 
 
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
