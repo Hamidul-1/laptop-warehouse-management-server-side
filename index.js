aaec296be5411c63c06d18434937ef1106e468fe
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-08 20:18:33 +0600

    added hero end point

--- index.js
+++ index.js
@@ -101,6 +101,10 @@ async function run() {
 
 run().catch(console.dir);
 
+app.get('/hero', (req, res) =>{
+    res.send('Hero meets hero ku')
+})
+
 
 app.get('/', (req, res) => {
     res.send('Running Genius Server');
